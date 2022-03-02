b6fa3f03a2b7308703c142f08faabcf331596463
wverastegui <wrojas@RCX-02331-NB.staff.ucn.muni.cz>
2022-03-02 11:03:25 +0100

    first change

--- src/reminder.py
+++ src/reminder.py
@@ -1,3 +1,6 @@
+from sys import prefix
+
+
 class PrefixedReminder:
     """This class acts as a base class for other types of reminders.
     Classes that subclass it should override the `self.text` property
@@ -5,3 +8,10 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    """Example
+    """
+    def __init__(self, text):
+        super().__init__(prefix="Please remind")
+        self.text = prefix + text
